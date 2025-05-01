38038a4605dff15c91ddc438fbf2ce5a40934b09
YoungJae98 <yj122198@naver.com>
2025-05-01 15:47:31 +0900

    test

--- src/pl_train.sh
+++ src/pl_train.sh
@@ -14,7 +14,6 @@ if [ ! -d $FOLDER ]; then
         mkdir $FOLDER
 fi
 
-
 OMP_NUM_THREADS=12 torchrun --nnodes=1 --nproc_per_node=8 --master_port=${m_port} pl_train.py  \
             -m ${model_name} -c vits/configs/vits_base.json -f ${training_file} -lr ${learning_rate} \
             -e ${epochs} -bs ${batch_size} -speak ${n_speakers}
